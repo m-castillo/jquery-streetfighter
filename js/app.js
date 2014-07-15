3dc7b09b0b2fb4054ba034384108ae136a3211c7
Manuel Castillo <mancast@gmail.com>
2014-07-15 14:30:09 -0500

    Logo, specific key event
    Added SF logo, special key event, and instructions for the user.

--- js/app.js
+++ js/app.js
@@ -10,33 +10,44 @@ $(document).ready(function() {
  .mousedown(function() {
     playHadouken();
     $('.ryu-ready').hide();
+    $('.ryu-still').hide();
     $('.ryu-throwing').show();
     $('.hadouken').finish().show()
     .animate(
-      {'left': '300px'},
+      {'left': '550px'},
        500,
        function() {
    		 $(this).hide();
    		 $(this).css('left', '-212px');
  		 }
-	);
+	 );
   })
   .mouseup(function() {
     $('.ryu-throwing').hide();
+    $('.ryu-still').hide();
     $('.ryu-ready').show();
-  });
- $("body").keydown(function(event) {
- 	$(".ryu-ready").hide();
- 	$(".ryu-cool").show();
-    })
- 	.keyup(function(event) {
- 	$(".ryu-cool").hide();
- 	$(".ryu-ready").show();
- 	})
-});
+    });
+})
+
 
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
+
+$(document).ready(function() {
+  $(document).keydown(function(e){
+    if (e.which == 88) { 
+       $('.ryu-ready').hide();
+       $('.ryu-still').hide();
+       $('.ryu-cool').show(); //note to mentor: if keyup event is .ryu-ready, image appears below the .ryu div. if reloading using chrome reload button, it does not happen
+     }
+  })
+  .keyup(function(e){
+    $('.ryu-cool').hide();
+    $('.ryu-ready').hide(); 
+    $('.ryu-still').show();
+  });
+})
+
